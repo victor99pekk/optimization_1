5b804b4c15d4b119f7c4d3bd3228332a280a8997
Victor Pekkari <victor.pekkari@gmail.com>
2024-10-21 21:57:08 -0700

    ny

--- hw2/GD.py
+++ hw2/GD.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 def gradient(A:np.array, b:np.array, x:np.array):
-    return 2 * np.dot(A.T, (np.dot(A, x) - b))
+    # return 2 * np.dot(A.T, (np.dot(A, x) - b))
 
     return 2 * np.dot((np.dot(A, x) - b).T, A)
 
@@ -32,25 +32,14 @@ def function(A:np.array, b:np.array, x:np.array, dim:int=999):
         sum += (A[i][0] * x[0] + A[i][1] * x[1] - 1) ** 2
     return sum
 
-def found_min(x:np.array, stop_criteria=1e-1):
-    for value in x:
-        if abs(value) > stop_criteria:
-            return False
-    return True
-    return True not in {(value > stop_criteria) for value in x}
-
 
 dim = 999
-x = np.array([random.uniform(0, 200) for _ in range(2)])
+x = np.array([random.uniform(0, 1) for _ in range(2)])
 A = get_A()
 b = np.array([1 for _ in range(dim)])
 
-while np.linalg.norm(gradient(A, b, x)) > 10000:
-    print(x)
-    print(learning_rate(A))
-    print(gradient(A, b, x))
-    print()
-
+# while np.linalg.norm(gradient(A, b, x)) > 1e-2:
+for i in range(10):
     x -= learning_rate(A) * gradient(A, b, x)
 
 print_result(A, b, x)
