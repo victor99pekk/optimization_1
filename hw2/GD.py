5ee9ee4024239241492f0f01716c97b492c3f3ba
Victor Pekkari <victor.pekkari@gmail.com>
2024-10-22 09:08:01 -0700

    tror allt funkar

--- hw2/GD.py
+++ hw2/GD.py
@@ -4,15 +4,16 @@ import pandas as pd
 
 
 
+
 def gradient(A:np.array, b:np.array, x:np.array):
     # return 2 * np.dot(A.T, (np.dot(A, x) - b))
-
     sum1 = 0
     sum2 = 0
     for i in range(999):
-        sum1 = 4 * ((A[i][0]*x[0] + A[i][1]*x[1]-1)**2) * A[i][0]*x[0]
-        sum2 = 4 * ((A[i][0]*x[0] + A[i][1]*x[1]-1)**2) * A[i][1]*x[1]
-    return np.array([sum1, sum2])
+        inner_product = (A[i][0] * x[0] + A[i][1] * x[1] - 1)
+        sum1 += 2 * inner_product * A[i][0]
+        sum2 += 2 * inner_product * A[i][1]
+    return np.array([sum1 / 999, sum2 / 999])
 
     return 2 * np.dot((np.dot(A, x) - b).T, A)
 
@@ -47,11 +48,17 @@ x = np.array([random.uniform(0, 1) for _ in range(2)])
 A = get_A()
 b = np.array([1 for _ in range(dim)])
 
-while np.linalg.norm(gradient(A, b, x)) > 1e-3:
-    print(function(A, b, x))
-    print(x)
-    print(learning_rate(A) * gradient(A, b, x))
+# print(function(A, b, x), x)
+# print(gradient(A, b, x), "\n")
+# x -= learning_rate(A) * gradient(A, b, x)
+# print(function(A, b, x), x, "\n")
+
+
+while np.linalg.norm(gradient(A, b, x)) > 1e-5:
+    # print(function(A, b, x))
+    # print(x)
+    # print(learning_rate(A) * gradient(A, b, x))
     x -= learning_rate(A) * gradient(A, b, x)
-    print(x, "\n")
+    # print(x, "\n")
 
 print_result(A, b, x)
